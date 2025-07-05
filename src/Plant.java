574bf88a6d3f4f283cb9503ebd7b29b09d11fa30
marekmaly <marcopiccolo007@gmail.com>
2025-07-05 23:12:23 +0200

    trida Plant (Ukol 1) - oprava chyby v konstruktoru (pridani default poznamky) a pridani zakladnich pristupovych metod (gettery a settery)

--- src/Plant.java
+++ src/Plant.java
@@ -6,38 +6,97 @@ public class Plant {
 
     //atributty tridy
     private String name;
-    private List<String> notes = new ArrayList<>();
-    //pokud ma poznamka v kolekci nest v sobe vice informaci nez jednoduchy text,
-    //tak mozne vytvorit novou tridu Note
+    private List<String> notes; //pokud ma poznamka v kolekci nest v sobe vice informaci nez jednoduchy text,
+    //tak mozne vytvorit novou tridu Note v projektu
     private LocalDate planted;
     private LocalDate watering;
     private int frequencyOfWatering;
 
     //konstruktory
-    //1. jeden pro nastavení všech atributů
-    public Plant(String name, List notes, LocalDate planted, LocalDate watering, int frequencyOfWatering) {
+    //1. konstruktor pro nastavení všech atributů
+    public Plant(String name, List<String> notes, LocalDate planted, LocalDate watering, int frequencyOfWatering) {
         this.name = name;
         this.notes = notes;
         this.planted = planted;
         this.watering = watering;
         this.frequencyOfWatering = frequencyOfWatering;
     }
-    //2. druhý nastaví jako poznámku prázdný řetězec a datum zasazení i datum poslední zálivky nastaví na dnešní datum
-    public Plant (String name, int frequencyOfWatering) {
-        this(name, notes.add(""), LocalDate.now(), LocalDate.now(), frequencyOfWatering);
+
+    //2. kontruktor, který nastaví jako poznámku prázdný řetězec a datum zasazení i datum poslední zálivky nastaví na dnešní datum
+    public Plant(String name, int frequencyOfWatering) {
+        this.name = name;
+        this.notes = new ArrayList<>();
+        this.notes.add("");
+        this.planted = LocalDate.now();
+        this.watering = LocalDate.now();
+        this.frequencyOfWatering = frequencyOfWatering;
     }
-    //3. třetí nastaví totéž co druhý a navíc výchozí frekvenci zálivky na 7 dnů
-    // ad 3. Uživatel tedy zadá pouze název rostliny
-    public Plant (String name) {
-        this(name, notes.add(""), LocalDate.now(), LocalDate.now(), 7);
+    //3. stejný konstruktor jako čílo 2. a navíc nastaví výchozí frekvenci zálivky na 7 dnů
+    public Plant(String name) {
+        this.name = name;
+        this.notes = new ArrayList<>();
+        this.notes.add("test poznamka");
+        this.planted = LocalDate.now();
+        this.watering = LocalDate.now();
+        this.frequencyOfWatering = 7;
     }
 
-    //pristupove metody
-    //UKOL - Vytvoř výchozí přístupové metody pro všechny atributy (?? asi mysleno jen getery a settery)
-    //1. getWateringInfo()
-    //ad 1. Vrátí textovou informaci obsahující název květiny, datum poslední zálivky a datum doporučené další zálivky.
-    //2. doWateringNow()
-    //ad 2. Nastaví datum poslední zálivky na dnešní den.
+    //pristupove metody (gettery a settery)
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<String> notes) {
+        this.notes = notes;
+    }
+
+    public void setNote(String note) {
+        notes.add(note);
+    }
+
+    public LocalDate getPlanted() {
+        return planted;
+    }
+
+    public void setPlanted(LocalDate planted) {
+        this.planted = planted;
+    }
+
+    public LocalDate getWatering() {
+        return watering;
+    }
+
+    public void setWatering(LocalDate watering) {
+        this.watering = watering;
+    }
+
+    public int getFrequencyOfWatering() {
+        return frequencyOfWatering;
+    }
+
+    public void setFrequencyOfWatering(int frequencyOfWatering) {
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+
+    //pristupove metody (getWateringInfo)
+    //Vrátí textovou informaci obsahující název květiny, datum poslední zálivky
+    //a datum doporučené další zálivky.
+
+
+
+
+    //pristupove metody (doWateringNow())
+    //Nastaví datum poslední zálivky na dnešní den.
+
 
 
 }
